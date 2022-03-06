0eb91a6964b534c648e5650552630050492a869d
Ranidhumma <ranidhumma8@gmail.com>
2022-03-06 16:18:37 +0530

    welcome

---
+++ src/com/java/Main.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome");
+		Integer x = 5, y = 15, z = 3;
+		Maximum.testMaximum(x, y, z);
+	}
+
+}
---
+++ src/com/java/Maximum.java
@@ -0,0 +1,35 @@
+package com.java;
+
+public class Maximum {
+
+	int x, y, z;
+
+	/* created constructor */
+
+	public Maximum(int x, int y, int z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	/* checking max value of three Integer */
+
+	public static Integer testMaximum(Integer x, Integer y, Integer z) {
+
+		Integer max = x;
+
+		if (y.compareTo(max) > 0)
+			max = y;
+
+		if (z.compareTo(max) > 0)
+			max = z;
+		printMax(x, y, z, max);
+		return max;
+
+	}
+
+	public static void printMax(Integer x, Integer y, Integer z, Integer max) {
+		System.out.printf("The Maximum value is: " + max);
+	}
+
+}
